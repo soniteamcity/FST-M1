c49c758deb9d1ae072a2379370cbb4583f63f01f
soniteamcity <77786433+soniteamcity@users.noreply.github.com>
2021-07-27 10:31:02 +0530

    Add files via upload

---
+++ Java/Activity1.java
@@ -0,0 +1,20 @@
+package activities;
+
+public class Activity1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    
+		Car i10 = new Car();
+		
+		i10.make=2014;
+		i10.color="black";
+		i10.transmission="Manual";
+		
+		i10.displayCharacteristics();
+		i10.accelarate();
+		i10.brake();
+		
+	}
+
+}
